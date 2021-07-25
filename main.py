bef38cf5814b6c0038b6bd175c0db723651204bc
tadinhkien99 <66372098+tadinhkien99@users.noreply.github.com>
2021-07-25 19:41:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ app= Flask(__name__)
 def index():
   data = pd.read_csv('Analytics4Dataset.csv', error_bad_lines=False)
   data = data[['Complaint description']]
-  embeddings_2 = np.load('embeddings.npy')
-  # model2 = torch.load('model.pth')
-#   model2 = pickle.load(open("model.sav", 'rb'))
-
-  
-#   topics, probabilities = model2.fit_transform(data['Complaint description'], embeddings_2)
-#   result = model2.get_topic(3)
-
-  return "model" 
-#   return str(result)
+  embeddings = np.load('embeddings.npy')  
+  model2 = pickle.load(open("model.sav", 'rb'))  
+  topics, probabilities = model2.fit_transform(data['Complaint description'], embeddings)
+  result = model2.get_topic(3)
+  return str(result)
